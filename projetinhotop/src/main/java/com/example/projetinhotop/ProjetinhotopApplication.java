f865cd9147fdc7b562877662dd942562052c95b4
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2021-07-15 20:36:06 -0300

    start

---
+++ projetinhotop/src/main/java/com/example/projetinhotop/ProjetinhotopApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetinhotop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetinhotopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetinhotopApplication.class, args);
+	}
+
+}
