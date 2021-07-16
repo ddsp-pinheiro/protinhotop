b3befa315b2ab5d22c724018d5b33bfc30d30b2d
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2021-07-16 11:45:21 -0300

    alterando getters e setters com lombok

--- projetinhotop/src/main/java/resources/UserResources.java
+++ projetinhotop/src/main/java/resources/UserResources.java
@@ -1,5 +1,17 @@
 package resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dto.UserDTO;
+
+@RequestMapping(value = "/users")
 public interface UserResources {
+
+    @PostMapping
+    ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO);
+
 
 }
